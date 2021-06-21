068836975249d1e1dae07e4bfb10052b5bf10465
Liliya Iskanderova <liliyaiskanderova@gmail.com>
2021-06-21 16:17:07 +0300

    HW2, third attempt locators are corrected

--- src/test/java/ru/training/at/hw2/ex1/Exercise1Tests.java
+++ src/test/java/ru/training/at/hw2/ex1/Exercise1Tests.java
@@ -35,7 +35,8 @@ public class Exercise1Tests {
     @Test
     public void exercise1Test() {
         //        1. Open test site
-        driver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
+        String url = "https://jdi-testing.github.io/jdi-light/index.html";
+        driver.navigate().to(url);
 
         //        2. Assert Browser title
         assertEquals(driver.getTitle(), "Home Page");
@@ -57,55 +58,54 @@ public class Exercise1Tests {
         //        5. Assert that four menu items with proper headers are displayed
         //        Home
         WebElement homeMenuHead =
-            driver.findElement(By.cssSelector("[class=\"uui-header dark-gray\"] a[href=\"index.html\"]"));
+            driver.findElement(By.xpath("//a[text()='Home']"));
         assertEquals(homeMenuHead.getText(), "HOME");
         //        Contact form
         WebElement contactFormMenuHead =
-            driver.findElement(By.cssSelector("[class=\"uui-header dark-gray\"] a[href=\"contacts.html\"]"));
+            driver.findElement(By.xpath("//a[text()='Contact form']"));
         assertEquals(contactFormMenuHead.getText(), "CONTACT FORM");
-        //        Service
+        //        Service xpath //a[text()=' Service '] isn't working
         WebElement servicesMenuHead =
-            driver.findElement(By.cssSelector("[class=\"uui-header dark-gray\"] a[class=\"dropdown-toggle\"]"));
+            driver.findElement(By.cssSelector("a[data-toggle=\"dropdown\"]"));
         assertEquals(servicesMenuHead.getText(), "SERVICE");
         //        Metals & Colors
         WebElement metalsAndColorsMenuHead =
-            driver.findElement(By.cssSelector("[class=\"uui-header dark-gray\"] a[href=\"metals-colors.html\"]"));
+            driver.findElement(By.xpath("//a[text()='Metals & Colors']"));
         assertEquals(metalsAndColorsMenuHead.getText(), "METALS & COLORS");
 
         //        6. Assert that there are 4 images on the Index Page and they are displayed
         //        Image 1
         driver.findElement(
-            By.cssSelector("div[class=\"row clerafix benefits\"] > div:nth-child(1) div[class=\"benefit-icon\"]"));
+            By.xpath("//span[@class=\"icons-benefit icon-practise\"]"));
         //        Image 2
         driver.findElement(
-            By.cssSelector("div[class=\"row clerafix benefits\"] > div:nth-child(2) div[class=\"benefit-icon\"]"));
+            By.xpath("//span[@class=\"icons-benefit icon-custom\"]"));
         //        Image 3
         driver.findElement(
-            By.cssSelector("div[class=\"row clerafix benefits\"] > div:nth-child(3) div[class=\"benefit-icon\"]"));
+            By.xpath("//span[@class=\"icons-benefit icon-multi\"]"));
         //        Image 4
         driver.findElement(
-            By.cssSelector("div[class=\"row clerafix benefits\"] > div:nth-child(4) div[class=\"benefit-icon\"]"));
+            By.xpath("//span[@class=\"icons-benefit icon-multi\"]"));
 
         //        7. Assert that there are 4 texts on the Index Page under icons and they have proper text
         //        Text 1
         WebElement textUnderMicroscope =
             driver.findElement(
-                By.cssSelector("div[class=\"row clerafix benefits\"] > div:nth-child(1) span[class=\"benefit-txt\"]"));
+                By.xpath("//span[contains(text(), 'To include good practices')]"));
         assertEquals(textUnderMicroscope.getText(), "To include good practices\nand ideas from successful\n"
             + "EPAM project");
         //        Text 2
         WebElement textUnderHeadphones =
-            driver.findElement(By.cssSelector("div[class=\"row clerafix benefits\"] > div:nth-child(2) "
-                + "span[class=\"benefit-txt\"]"));
+            driver.findElement(By.xpath("//span[contains(text(), 'To be flexible')]"));
         assertEquals(textUnderHeadphones.getText(), "To be flexible and\ncustomizable");
         //        Text 3
         WebElement textUnderMonitor =
             driver.findElement(
-                By.cssSelector("div[class=\"row clerafix benefits\"] > div:nth-child(3) span[class=\"benefit-txt\"]"));
+                By.xpath("//span[contains(text(), 'To be multiplatform')]"));
         assertEquals(textUnderMonitor.getText(), "To be multiplatform");
         //        Text 4
         WebElement textUnderRocket = driver.findElement(
-            By.cssSelector("div[class=\"row clerafix benefits\"] > div:nth-child(4) span[class=\"benefit-txt\"]"));
+            By.xpath("//span[contains(text(), 'Already have good base')]"));
         assertEquals(textUnderRocket.getText(), "Already have good base\n(about 20 internal and\n"
             + "some external projects),\nwish to get more…");
 
@@ -125,23 +125,23 @@ public class Exercise1Tests {
         //        (duplicate for step 5 except “Elements packs” section
         //        Home
         WebElement homeMenuLeft =
-            driver.findElement(By.cssSelector("ul[class=\"sidebar-menu left\"] a[href=\"index.html\"]"));
+            driver.findElement(By.xpath("//span[text()='Home']"));
         assertEquals(homeMenuLeft.getText(), "Home");
         //        Contact form
         WebElement contactFormMenuLeft =
-            driver.findElement(By.cssSelector("ul[class=\"sidebar-menu left\"] a[href=\"contacts.html\"]"));
+            driver.findElement(By.xpath("//span[text()='Contact form']"));
         assertEquals(contactFormMenuLeft.getText(), "Contact form");
         //        Service
         WebElement serviceMenuLeft =
-            driver.findElement(By.cssSelector("ul[class=\"sidebar-menu left\"] > li[index=\"3\"]"));
+            driver.findElement(By.xpath("//span[text()='Service']"));
         assertEquals(serviceMenuLeft.getText(), "Service");
         //        Metals & Colors
         WebElement metalsAndColorsMenuLeft =
-            driver.findElement(By.cssSelector("ul[class=\"sidebar-menu left\"] a[href=\"metals-colors.html\"]"));
+            driver.findElement(By.xpath("//span[text()='Metals & Colors']"));
         assertEquals(metalsAndColorsMenuLeft.getText(), "Metals & Colors");
         //        Elements packs
         WebElement elementsPacksMenuLeft =
-            driver.findElement(By.cssSelector("ul[class=\"sidebar-menu left\"] > li[index=\"5\"]"));
+            driver.findElement(By.xpath("//span[text()='Elements packs']"));
         assertEquals(elementsPacksMenuLeft.getText(), "Elements packs");
     }
 }
--- src/test/java/ru/training/at/hw2/ex2/Exercise2Tests.java
+++ src/test/java/ru/training/at/hw2/ex2/Exercise2Tests.java
@@ -1,6 +1,7 @@
 package ru.training.at.hw2.ex2;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
@@ -34,7 +35,8 @@ public class Exercise2Tests {
     @Test
     public void exercise2Test() {
         //        1. Open test site
-        driver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
+        String url = "https://jdi-testing.github.io/jdi-light/index.html";
+        driver.navigate().to(url);
 
         //        2. Assert Browser title
         assertEquals(driver.getTitle(), "Home Page");
@@ -55,31 +57,31 @@ public class Exercise2Tests {
 
         //        5. Open through the header menu Service -> Different Elements Page
         WebElement serviceMenu =
-            driver.findElement(By.cssSelector("[class=\"uui-header dark-gray\"] a[class=\"dropdown-toggle\"]"));
+            driver.findElement(By.cssSelector("a[data-toggle=\"dropdown\"]"));
         serviceMenu.click();
         WebElement differentElementsMenu =
-            driver.findElement(By.cssSelector(
-                "ul[class=\"uui-navigation nav navbar-nav m-l8\"]  a[href=\"different-elements.html\"]"));
+            driver.findElement(By.xpath(
+                "//a[text()='Different elements']"));
         differentElementsMenu.click();
 
         //        6. Select checkboxes Water, Wind
         WebElement waterCheckbox =
             driver
-                .findElement(By.cssSelector("div[class=\"checkbox-row\"] > label:nth-child(1) > input[type=checkbox]"));
+                .findElement(By.cssSelector("label:nth-child(1) > input[type=checkbox]"));
         waterCheckbox.click();
         WebElement windCheckbox =
             driver
-                .findElement(By.cssSelector("div[class=\"checkbox-row\"] > label:nth-child(3) > input[type=checkbox]"));
+                .findElement(By.cssSelector("label:nth-child(3) > input[type=checkbox]"));
         windCheckbox.click();
 
         //        7. Select radio Selen
         WebElement selenRadio =
-            driver.findElement(By.cssSelector("div[class=\"checkbox-row\"] > label:nth-child(4) > input[type=radio]"));
+            driver.findElement(By.cssSelector("label:nth-child(4) > input[type=radio]"));
         selenRadio.click();
 
         //        8. Select in dropdown Yellow
         WebElement dropdown =
-            driver.findElement(By.cssSelector("div[class=\"colors\"] > select[class=\"uui-form-element\"]"));
+            driver.findElement(By.cssSelector("select[class=\"uui-form-element\"]"));
         dropdown.click();
         WebElement yellow =
             driver.findElement(By.xpath("//option[text()='Yellow']"));
@@ -91,21 +93,16 @@ public class Exercise2Tests {
         //    • for dropdown there is a log row and value is corresponded to the selected value
         //        Yellow
         WebElement colors =
-            driver.findElement(By.xpath("//li[contains(text(), 'Colors: value changed to Yellow')]"));
-        String colorsText = colors.getText().substring(9);
-        assertEquals(colorsText, "Colors: value changed to Yellow");
+            driver.findElement(By.xpath("//li[contains(text(), 'Yellow')]"));
+        assertTrue(colors.getText().contains("Colors: value changed to Yellow"));
         //        Selen - little bag. "Metal" does not start with the capital letter
-        //        also xpath finding by text isn't working
-        WebElement metal = driver.findElement(By.cssSelector("ul[class=\"panel-body-list logs\"] > li:nth-child(2)"));
-        String metalText = metal.getText().substring(9);
-        assertEquals(metalText, "metal: value changed to Selen");
+        WebElement metal = driver.findElement(By.xpath("//li[contains(text(), 'Selen')]"));
+        assertTrue(metal.getText().contains("metal: value changed to Selen"));
         //        Wind
-        WebElement wind = driver.findElement(By.xpath("//li[contains(text(), 'Wind: condition changed to true')]"));
-        String windText = wind.getText().substring(9);
-        assertEquals(windText, "Wind: condition changed to true");
+        WebElement wind = driver.findElement(By.xpath("//li[contains(text(), 'Wind')]"));
+        assertTrue(wind.getText().contains("Wind: condition changed to true"));
         //        Water
-        WebElement water = driver.findElement(By.xpath("//li[contains(text(), 'Water: condition changed to true')]"));
-        String waterText = water.getText().substring(9);
-        assertEquals(waterText, "Water: condition changed to true");
+        WebElement water = driver.findElement(By.xpath("//li[contains(text(), 'Water')]"));
+        assertTrue(water.getText().contains("Water: condition changed to true"));
     }
 }
